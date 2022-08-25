6f04a16d7201b3a0006eec068cb955e6bc6bd901
wangyu-0426 <wangyu_0426@qq.com>
2022-08-25 18:32:37 +0800

    '添加jest单测'

---
+++ tests/unit/hello.test.js
@@ -0,0 +1,31 @@
+import { mount } from '@vue/test-utils';
+import Component from '../../src/components/ComponentTest.vue';
+import Hello from '../../src/components/HelloWorld.vue';
+
+describe('Component', () => {
+    test('is a Vue instance', () => {
+        const wrapper = mount(Component, {
+            props: {
+                name: 'myName',
+            },
+        });
+        // expect(wrapper.classes()).toContain('bar')
+        expect(wrapper.vm.count).toBe(0);
+        const button = wrapper.find('button');
+        button.trigger('click');
+        expect(wrapper.vm.count).toBe(1);
+        expect(wrapper.find('.msg').text()).toBe('hello');
+        expect(wrapper.find('.name').text()).toBe('myName');
+        wrapper.unmount();
+    });
+    test("hello", () => {
+        const wrapper = mount(Hello, {
+            props: {
+                msg: "message!!!"
+            }
+        })
+        // console.log(wrapper)
+        // console.log(wrapper.html())
+        expect(wrapper.html()).toContain('message!!!')
+    })
+});
